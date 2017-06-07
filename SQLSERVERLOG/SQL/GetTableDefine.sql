8c279b18e7478eb28733e377cc389bbefc94befd
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-06-07 19:21:01 +0800

    first submit

---
+++ SQLSERVERLOG/SQL/GetTableDefine.sql
@@ -0,0 +1,5 @@
+﻿select obj.name,col.name, column_id, max_length, typ.name
+from sys.columns col join sys.objects obj on col.object_id = obj.object_id
+join sys.systypes typ on typ.xtype = col.user_type_id
+where obj.[type]='U' and obj.name ='<tableName>'
+order by column_id
---
+++ SQLSERVERLOG/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace SQLSERVERLOG
+{
+    public class Utility
+    {
+        private const string keyConnStr = "ConnStr";
+
+        public string TableDefineSql { get { return "GetDBLog.sql"; } }
+        public string DBLogSql { get { return "GetTableDefine"; } }
+
+        public string GetConfigConnStr()
+        {
+            var connStr = ConfigurationManager.AppSettings[keyConnStr];
+            return connStr;
+        }
+
+        public string GetSQLFromFile(string fileName)
+        {
+            var sqlStr = string.Empty;
+            using (var read = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "SQL\\" + fileName))
+            {
+                sqlStr = read.ToString().Trim();
+            }
+            return null;
+        }
+    }
+}
