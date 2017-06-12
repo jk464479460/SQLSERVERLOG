f62f2d4305334ac3959b6f907992e9184f55960f
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-06-12 17:25:42 +0800

    nvarchar, Look Log made

---
+++ SQLSERVERLOG/Test/TB-T5.sql
@@ -0,0 +1,21 @@
+﻿USE [Test]
+GO
+
+/****** Object:  Table [dbo].[t5]    Script Date: 06/12/2017 17:21:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[t5](
+	[id] [int] NOT NULL,
+	[age] [int] NOT NULL,
+	[name] [nvarchar](400) NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[t5] ADD  DEFAULT (NULL) FOR [name]
+GO
+
